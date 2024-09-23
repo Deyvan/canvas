1f05728c89bdde15e16fbfa0dbe21469efab590b
deyvan <deyvanthebest@protonmail.com>
2024-09-23 04:48:02 +0400

    test: More info

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
+// that file will be removed
+
 let canvas = require("../skia.linux-x64-gnu.node")
 
-for(;;)
-    console.log(canvas.getGPUs())
+for(let gpu of canvas.getGPUs()){
+    console.log(gpu, gpu.name)
+}
